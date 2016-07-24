d46d696688a530e190a6a847c39bd17b6eee4a29
Sara Piette <sepiette@gmail.com>
2016-07-24 14:43:12 -0400

    prime factor coded

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,41 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+
+        List<Integer> primeFactors = generate(315);
+        for(Integer factor: primeFactors){
+            System.out.print(factor+" ");
+        }
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        Set<Integer> primeSet = new HashSet<>();
+        if(n==1){
+            return new LinkedList<>();
+        }
+        while(n%2==0){
+            primeSet.add(2);
+            n /= 2;
+        }
+        for(int i=3; i<Math.sqrt(n);i+=2){
+            while(n%i==0){
+                primeSet.add(i);
+                n /= i;
+            }
+        }
+
+        if(n>2){
+            primeSet.add(n);
+        }
+        LinkedList<Integer> primes =  new LinkedList<Integer>();
+        primes.addAll(primeSet);
+        return primes;
     }
 }
