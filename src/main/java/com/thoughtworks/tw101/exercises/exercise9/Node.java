4a18df0fd484fc7509f9f69cd98a8f949dae4cb3
Sara Piette <sepiette@gmail.com>
2016-08-04 15:14:29 -0400

    re-factored exercise 9.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,55 +1,36 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Node {
     private String name;
-    private Node left;
-    private Node right;
+    private Node next;
 
     public Node(String name) {
         this.name = name;
-        this.left = null;
-        this.right = null;
+
+        this.next = null;
     }
 
     public void add(String nameOfNewNode) {
-        if (this.name.compareToIgnoreCase(nameOfNewNode) > 0){
-                if(this.left != null){
-                    this.left.add(nameOfNewNode);
-                }
-                else{
-                    this.left = new Node(nameOfNewNode);
-                }
-        }
-
-        else if(this.name.compareToIgnoreCase(nameOfNewNode) < 0){
-                if(this.right != null){
-                    this.right.add(nameOfNewNode);
-                }
-                else{
-                    this.right = new Node(nameOfNewNode);
-                }
+        if(this.next != null){
+            this.next.add(nameOfNewNode);
         }
         else{
-            return;
+            this.next = new Node(nameOfNewNode);
         }
     }
 
     public List<String> names() {
         LinkedList<String> nameList = new LinkedList();
-        nameList = inOrderTraverse(this,nameList);
+        Node currNode = this;
+        while(currNode != null){
+            nameList.add(currNode.name);
+            currNode = currNode.next;
+        }
+        Collections.sort(nameList);
         return nameList;
-    }
-    private LinkedList<String> inOrderTraverse(Node root, LinkedList<String> names){
-
-        if (root == null)  {return null;}
-
-        inOrderTraverse(root.left,names);
-        names.add(root.name);
-        inOrderTraverse(root.right,names);
-
-        return names;
     }
 }
